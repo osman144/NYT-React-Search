3ec7a19e5bb8beae10eb7ac162d75ff27c4fd8e8
osman144 <osman144@umn.edu>
2018-07-09 18:47:40 -0500

    app js components

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+import Jumbotron from './components/Jumbotron'
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container">
+          <Jumbotron />
+          {/* <Articles /> */}
+        </div>
       </div>
     );
   }
---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Jumbotron.css'
+const Jumbotron = () => (
+    <div className="jumbotron">
+      <h1 className="text-center">
+        <strong>
+          <i class="fa fa-newspaper-o"></i> New York Times Search
+        </strong>
+      </h1>
+    </div>
+);
+
+
+export default Jumbotron;
